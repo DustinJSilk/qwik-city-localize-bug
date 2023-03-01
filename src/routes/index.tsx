71b18a4cddabd236531e0720aa2eaf8e200bd9e7
Dustin Silk <dustin@staze.com>
2023-03-01 09:42:21 +0000

    first commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,25 @@
+import { component$ } from "@builder.io/qwik";
+import type { DocumentHead } from "@builder.io/qwik-city";
+import { Link } from "@builder.io/qwik-city";
+
+export default component$(() => {
+  return (
+    <div>
+      <h1>{$localize`Welcome`}</h1>
+
+      <Link class="mindblow" href="/flower/">
+        Blow my mind 🤯
+      </Link>
+    </div>
+  );
+});
+
+export const head: DocumentHead = {
+  title: "Welcome to Qwik",
+  meta: [
+    {
+      name: "description",
+      content: "Qwik site description",
+    },
+  ],
+};
